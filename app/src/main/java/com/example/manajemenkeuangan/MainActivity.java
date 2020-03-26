8b6877336a87c2ece3bda1b7f30445153f6a02e6
swordfk <khijabfatkhul@gmail.com>
2020-03-26 21:14:02 +0700

    1.0

---
+++ app/src/main/java/com/example/manajemenkeuangan/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.manajemenkeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        Intent i;
+        switch (selectedMode) {
+            case R.id.action_about:
+
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/manajemenkeuangan/catatanKeuangan.java
@@ -0,0 +1,14 @@
+package com.example.manajemenkeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class catatanKeuangan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catatan_keuangan);
+    }
+}
